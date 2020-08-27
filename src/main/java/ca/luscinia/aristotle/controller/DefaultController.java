e9007a7aa9574619c3303cc8e3187385be2ed57c
Varun Patel <varun@varunpatel.ca>
2020-08-27 10:40:26 -0400

    Add Health Check

--- src/main/java/ca/luscinia/aristotle/controller/DefaultController.java
+++ src/main/java/ca/luscinia/aristotle/controller/DefaultController.java
@@ -19,9 +19,11 @@
 
 package ca.luscinia.aristotle.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -59,4 +61,8 @@ public class DefaultController extends AristotleController {
         ModelAndView modelAndView = new ModelAndView("about-us");
         return modelAndView;
     }
+
+    @RequestMapping(path = "/health")
+    @ResponseStatus(HttpStatus.OK)
+    public void health() {}
 }
